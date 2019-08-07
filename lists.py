80982a7630268d184ebed3bdb34548d6e656fe2e
it.region11 <it.region11@gmail.com>
2019-08-07 11:39:03 +0700

    tuples_sets

--- lists.py
+++ lists.py
@@ -5,10 +5,31 @@ print(type(numbers))
 print(numbers)
 # Using a constructor
 num = list((1,2,3,4,5,6,7,8,9))
-fruits = ['Apples', 'Oranges', 'Grapes', 'Pears', 12, 21.39]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
 print(num)
 print(fruits)
 # Get value
 print(fruits[3])
 # Get len
 print(len(fruits))
+# Append to list
+fruits.append('Mangoes')
+print(fruits)
+# Remove from list
+fruits.remove('Pears')
+print(fruits)
+# Insert into position
+fruits.insert(4, 'Strawberries')
+print(fruits)
+# Remove from position
+fruits.pop(1)
+print(fruits)
+# Reverse list
+fruits.reverse()
+print(fruits)
+# Sort list
+fruits.sort()
+print(fruits)
+# Reverse list
+fruits.sort(reverse=True)
+print(fruits)
--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,20 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  
+# Simple tuple
+fruit_tuple = ('Apple', 'Orange', 'Mango', 'Grape')
+print(fruit_tuple)
+# Get single value
+print(fruit_tuple[0])
+# Tuples with one value must have trailing comma
+fruit_tuple2 = ('Apple',)
+print(fruit_tuple2)
+# Get lenght of tuple
+print(len(fruit_tuple))
 
+# A Set is a collection which is unordered and unindexed. * No duplicate members.*
 
-# A Set is a collection which is unordered and unindexed. No duplicate members.
+# Create set
+fruit_set = {'Apple', 'Orange', 'Mango', 'Grape', 'Apple'}
+print(fruit_set)
+# Check if in set
+print('Apple' in fruit_set)
